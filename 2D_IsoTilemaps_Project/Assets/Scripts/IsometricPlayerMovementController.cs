61e7bbc3fc2f70949d410a6e7137cdab7fcaed78
NolanYou <nolan@Nolans-MacBook-Pro.local>
2022-07-29 16:17:47 -0700

    modified physics engine for grappling hook

--- 2D_IsoTilemaps_Project/Assets/Scripts/IsometricPlayerMovementController.cs
+++ 2D_IsoTilemaps_Project/Assets/Scripts/IsometricPlayerMovementController.cs
@@ -1,33 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class IsometricPlayerMovementController : MonoBehaviour
 {
-
-    public float movementSpeed = 1f;
+    const float movementSpeed = 10;
+    
     IsometricCharacterRenderer isoRenderer;
 
     Rigidbody2D rbody;
-
+ 
     private void Awake()
     {
         rbody = GetComponent<Rigidbody2D>();
         isoRenderer = GetComponentInChildren<IsometricCharacterRenderer>();
+        rbody.isKinematic = false;
+        rbody.drag = 5;
+        rbody.useAutoMass = true;
+        rbody.velocity = new Vector2(0,0);
     }
 
 
     // Update is called once per frame
     void FixedUpdate()
-    {
+    {   
         Vector2 currentPos = rbody.position;
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
+        
+        
         inputVector = Vector2.ClampMagnitude(inputVector, 1);
-        Vector2 movement = inputVector * movementSpeed;
-        Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
+
+        
+
+        Vector2 movement = movementSpeed * Time.deltaTime * inputVector;
+        rbody.drag = Math.Max(5, rbody.velocity.magnitude * 0.2f);
+
+        rbody.AddForce(movement);
+        // rbody.velocity = movement;
+        // Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
         isoRenderer.SetDirection(movement);
-        rbody.MovePosition(newPos);
+        // rbody.MovePosition(newPos);
+        Debug.Log("Schmoobment speed" + rbody.GetPointVelocity(new Vector2(0,0)));
     }
 }
